ae4aafc513dbe99e328d5f8d2e35f95de66ae894
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-08-29 17:05:47 +0400

    strings is practiced

---
+++ Jed/string/app.js
@@ -0,0 +1,17 @@
+//! T1
+let cumle1 = "today we're gonna learn strings on JavaScript ";
+let firstLetter = cumle1.slice(0, 1);
+let netice1 = firstLetter.toUpperCase()
+for (i = 1; i < cumle1.length; i++) {
+    netice1 += cumle1[i];
+}
+console.log(netice1)
+
+//T2
+let cumle2 = "today we're gonna learn strings on JavaScript ";
+let firstO = cumle2.indexOf('o');
+let lastO = cumle2.indexOf('o', firstO);
+let netice2 = cumle2.slice(firstO, lastO + 1);
+// let netice2 = cumle2.slice(1, lastO);
+console.log(netice2);
+
---
+++ Jed/string/app2.js
@@ -0,0 +1,5 @@
+const cumle = "biz.bu.gun.getmek.ugrunda.mucadele.aparmaga.heveslenmek.ucun.calisacagiq"
+let changeCumle1 = cumle.replaceAll('.', ' ');
+let changeCumle2 = changeCumle1.replaceAll('mek', 'mək')
+
+console.log(changeCumle2);
