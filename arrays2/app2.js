5c012aea5a98f7b9dd62aced2bfc28e7b5cc2a43
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-09-07 13:04:43 +0400

    some changes i array practising

--- arrays2/app2.js
+++ arrays2/app2.js
@@ -1,8 +1,5 @@
 function multiply(...numbers) {
-    const product = numbers.reduce((a, b) => {
-        return a * b
-    }, 1)
-    return product
-
+    const product = numbers.reduce((a, b) => a * b, 1);
+    return `the product of numbers of ${numbers} is ${product}`;
 }
 console.log(multiply(12, 2, 5));
